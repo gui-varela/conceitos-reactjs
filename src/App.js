cc622340bd99fa28afd592e03c208b0a82ffca58
Guilherme Varela <guilhermevareladossantos@gmail.com>
2020-08-13 21:39:17 -0300

    conect api-frontend

--- src/App.js
+++ src/App.js
@@ -1,29 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    api.get('repositories')
+      .then(response => setRepositories(response.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  function handleTitleChange(event) {
+    const title = event.target.value;
+    setTitle(title);
+  }
+
+  async function handleAddRepository(event) {
+    event.preventDefault();
+
+    const inputField = document.getElementById('title');
+
+    const title = inputField.value;
+
+    const repositoryData = {
+      title: title,
+      url: "http://github.com/desafio2",
+      techs: ["Nodejs", "express"]
+    }
+
+    const response = await api.post('repositories', repositoryData);
+
+    const createdRepository = response.data;
+
+    if (createdRepository) {
+      setRepositories([...repositories, createdRepository]);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const repositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+    if (repositoryIndex >= 0) {
+      const updatedRepositoryList = [...repositories];
+      
+      updatedRepositoryList.splice(repositoryIndex, 1);
+
+      setRepositories(updatedRepositoryList);
+
+      await api.delete(`repositories/${id}`);
+    }
   }
 
   return (
     <div>
+      <form>
+        <input
+          id="title"
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+          placeholder="Repository Title"
+        />
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </form>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
